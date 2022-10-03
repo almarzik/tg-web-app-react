4320d320c8ced70eef871e5987fabab6a9dd2a52
Martsinovkiy Aleksey <almarzik@mail.ru>
2022-10-03 22:26:11 +0300

    commit

--- src/hooks/useTelegram.js
+++ src/hooks/useTelegram.js
@@ -1,20 +1,19 @@
 const tg = window.Telegram.WebApp;
 export function useTelegram() {
-      
-      const onClose = () => {
-           tg.close();
-      }
-      const onToggleButton = () => {
-           if(tg.MainButton.isVisible) {
-            tg.MainButton.show();
-           }else {
-            tg.MainButton.hide();
-           }
-      }
-      return {
-            onClose,
-            onToggleButton,
-            tg,
-            user:tg.initDataSafe?user,
-      }
+     const onClose = () => {
+          tg.close();
+     };
+     const onToggleButton = () => {
+          if (tg.MainButton.isVisible) {
+               tg.MainButton.show();
+          } else {
+               tg.MainButton.hide();
+          }
+     };
+     return {
+          onClose,
+          onToggleButton,
+          tg,
+          user: tg.initDataUnsafe?.user,
+     };
 }
